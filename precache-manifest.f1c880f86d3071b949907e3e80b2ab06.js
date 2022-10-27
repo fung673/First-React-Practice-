e4d45b60cd63f4005b1d7c21b59af7e47ed37e28
unknown <alexfung1234@gmail.com>
2022-10-27 20:15:22 +0800

    Updates

---
+++ precache-manifest.f1c880f86d3071b949907e3e80b2ab06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a86dfada02c38969b10d",
+    "url": "/First-React-Practice-/static/css/main.fcabbe18.chunk.css"
+  },
+  {
+    "revision": "a86dfada02c38969b10d",
+    "url": "/First-React-Practice-/static/js/main.a86dfada.chunk.js"
+  },
+  {
+    "revision": "7fe6c5c56602ae9f819b",
+    "url": "/First-React-Practice-/static/js/1.7fe6c5c5.chunk.js"
+  },
+  {
+    "revision": "a07670c95ef79f0b1e41",
+    "url": "/First-React-Practice-/static/js/runtime~main.a07670c9.js"
+  },
+  {
+    "revision": "762a1c61a12e8039e9cce842b5f16dd4",
+    "url": "/First-React-Practice-/static/media/dog.762a1c61.jpg"
+  },
+  {
+    "revision": "3ec30ed203daed78688bb5aebd08dd31",
+    "url": "/First-React-Practice-/index.html"
+  }
+];
